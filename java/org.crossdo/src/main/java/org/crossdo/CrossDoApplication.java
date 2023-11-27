9ac8af2f44b22e8b97696f391dbabadcf04292c7
赖嘉康 <laijiakang@kuaishou.com>
2023-11-27 21:10:52 +0800

    【init java】

---
+++ java/org.crossdo/src/main/java/org/crossdo/CrossDoApplication.java
@@ -0,0 +1,13 @@
+package org.crossdo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrossDoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrossDoApplication.class, args);
+    }
+
+}
---
+++ java/org.crossdo/src/test/java/org/crossdo/CrossDoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.crossdo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrossDoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
