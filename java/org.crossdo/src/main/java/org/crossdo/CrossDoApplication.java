dc3d8c2e71e40e4fc4f62519dd27e3378ca2cdac
DollyDays <2475442190@qq.com>
2023-11-27 21:24:03 +0800

    【init java】

--- java/org.crossdo/src/main/java/org/crossdo/CrossDoApplication.java
+++ java/org.crossdo/src/main/java/org/crossdo/CrossDoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CrossDoApplication {
 
     public static void main(String[] args) {
+        // 启动嵌入式的 Tomcat 并初始化 Spring 环境及其各 Spring 组件
         SpringApplication.run(CrossDoApplication.class, args);
     }
 
